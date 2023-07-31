bead4729f617db561fc56e51650be805e450b450
zaneris123 <zaneris123@gmail.com>
2023-07-31 10:57:37 +0100

    first commit, cloud function created

---
+++ functions/index.js
@@ -0,0 +1,30 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccount.json");
+require("dotenv").config();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.DATABASEURL,
+});
+
+const db = admin.database();
+const ref = db.ref("/processed_transactions");
+const balanceRef = ref.child("currentbalance");
+
+exports.onCreateValue = functions
+    .region("europe-west3") // Set region here
+    .database.ref("/transactions/{valueId}")
+    .onCreate((snapshot, context) => {
+      const newTransaction = snapshot.val();
+      const transactionRef = ref.child(newTransaction.data.id);
+      if (newTransaction.type == "transaction.created") {
+        transactionRef.once("value").then((snapshot)=>{
+          if (!snapshot.exists()) {
+            transactionRef.set(newTransaction);
+            balanceRef.transaction((currentValue)=>{
+              return newTransaction.data.amount + (currentValue);
+            });
+          }
+        });
+      }
+    });
